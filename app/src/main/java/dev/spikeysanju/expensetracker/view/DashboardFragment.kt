d1fe9481093768355f72f111895640d831cf6867
Sanju S <spikeysanju98@gmail.com>
2021-01-24 00:00:46 +0530

    Complete total dashboard count
    Signed-off-by: Spikeysanju <spikeysanju98@gmail.com>

--- app/src/main/java/dev/spikeysanju/expensetracker/view/DashboardFragment.kt
+++ app/src/main/java/dev/spikeysanju/expensetracker/view/DashboardFragment.kt
@@ -7,19 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import dev.spikeysanju.expensetracker.R
 import dev.spikeysanju.expensetracker.databinding.FragmentDashboardBinding
 import dev.spikeysanju.expensetracker.db.AppDatabase
+import dev.spikeysanju.expensetracker.model.Transaction
 import dev.spikeysanju.expensetracker.repo.TransactionRepo
 import dev.spikeysanju.expensetracker.utils.ViewState
 import dev.spikeysanju.expensetracker.utils.viewModelFactory
+import dev.spikeysanju.expensetracker.view.adapter.TransactionAdapter
 import dev.spikeysanju.expensetracker.view.base.BaseFragment
 import dev.spikeysanju.expensetracker.viewmodel.TransactionViewModel
+import indianRupee
 import kotlinx.coroutines.flow.collect
 
 class DashboardFragment :
     BaseFragment<FragmentDashboardBinding, TransactionViewModel>() {
-
+    private lateinit var transactionAdapter: TransactionAdapter
     private val transactionRepo by lazy {
         TransactionRepo(AppDatabase.invoke(applicationContext()))
     }
@@ -31,17 +38,102 @@ class DashboardFragment :
         super.onViewCreated(view, savedInstanceState)
         initViews()
         observeData()
+        setupRV()
+        observeDashboardTotal()
+        swipeToDelete()
+    }
+
+    private fun setupRV() = with(binding) {
+        transactionAdapter = TransactionAdapter()
+        transactionRv.apply {
+            adapter = transactionAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun swipeToDelete() {
+        // init item touch callback for swipe action
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // get item position & delete notes
+                val position = viewHolder.adapterPosition
+                val transaction = transactionAdapter.differ.currentList[position]
+                val transactionItem = Transaction(
+                    transaction.title,
+                    transaction.amount,
+                    transaction.transactionType,
+                    transaction.tag,
+                    transaction.date,
+                    transaction.note,
+                    transaction.createdAt,
+                    transaction.id
+                )
+                viewModel.deleteTransaction(transactionItem)
+                Snackbar.make(
+                    binding.root,
+                    getString(R.string.success_transaction_delete),
+                    Snackbar.LENGTH_LONG
+                )
+                    .apply {
+                        setAction("Undo") {
+                            viewModel.insertTransaction(
+                                transactionItem
+                            )
+                        }
+                        show()
+                    }
+            }
+        }
+
+        // attach swipe callback to rv
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(binding.transactionRv)
+        }
+    }
+
+    private fun observeDashboardTotal() = lifecycleScope.launchWhenStarted {
+        viewModel.uiState.collect { uiState ->
+            when (uiState) {
+                ViewState.Loading -> TODO()
+                ViewState.Empty -> TODO()
+                is ViewState.Success -> {
+                    onTotalTransactionLoaded(uiState.transaction)
+                }
+                is ViewState.Error -> TODO()
+            }
+        }
+    }
+
+    private fun onTotalTransactionLoaded(transaction: List<Transaction>) = with(binding) {
+        val (totalIncome, totalExpense) = transaction.partition { it.transactionType == "Income" }
+        val income = totalIncome.sumByDouble { it.amount }
+        val expense = totalExpense.sumByDouble { it.amount }
+        incomeCardView.total.text = indianRupee(income)
+        expenseCardView.total.text = indianRupee(expense)
+        totalBalanceView.totalBalance.text = indianRupee(income - expense)
+
     }
 
     private fun observeData() {
 
-        lifecycleScope.launchWhenCreated {
+        lifecycleScope.launchWhenStarted {
             viewModel.uiState.collect { uiState ->
                 when (uiState) {
                     is ViewState.Loading -> {
                     }
                     is ViewState.Success -> {
-                        toast("Transaction is ${uiState.transaction.last().title}")
+                        onTransactionLoaded(uiState.transaction)
                     }
                     is ViewState.Error -> {
                         toast("Error")
@@ -52,6 +144,10 @@ class DashboardFragment :
                 }
             }
         }
+    }
+
+    private fun onTransactionLoaded(list: List<Transaction>) {
+        transactionAdapter.differ.submitList(list)
     }
 
     private fun initViews() = with(binding) {
