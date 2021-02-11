598d24a257b80ac74a15b4f406bfa90c585d0745
maifeeulasad <maifeeulasad@gmail.com>
2021-02-11 18:46:16 +0600

    setup for stable android studio + stable android libraries//wip

--- app/src/main/java/dev/spikeysanju/expensetracker/view/dashboard/DashboardFragment.kt
+++ app/src/main/java/dev/spikeysanju/expensetracker/view/dashboard/DashboardFragment.kt
@@ -93,7 +93,7 @@ class DashboardFragment :
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
             ): Boolean {
                 return true
             }
